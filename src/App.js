536fe6238caab7cb15ccee0afd78759d05b95535
sturgeon.landon <sturgeon.landon@gmail.com>
2021-03-30 23:45:09 -0800

    fixed import error, adjust image size

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import React, { Component } from "react";
-import { CardList } from "../src/Components/CardList/CardList.component";
-import { SearchBar } from "../src/Components/SearchBar/SearchBar.component";
+import CardList from "./Components/CardList/CardList.component";
+import SearchBar from "./Components/SearchBar/SearchBar.component";
 import "./App.css";
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
 
     this.state = {
-      monsters = [],
-      searchField: ""
+      monsters: [],
+      searchField: "",
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value });
-  }
+  };
 
   render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
+    const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
+    );
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
-        <SearchBar placeholder="search monsters"
-        handleChange={ this.handleChange}
+        <SearchBar
+          placeholder="search monsters"
+          handleChange={this.handleChange}
         />
-        <CardList monsters={ filteredMonsters}></CardList>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
--- src/Components/Card/Card.component.js
+++ src/Components/Card/Card.component.js
@@ -5,7 +5,9 @@ const Card = (props) => (
   <div className="card-container">
     <img
       alt="monster"
-      src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`}
+      src={`https://robohash.org/${props.monster.id}?set=set1`}
+      height="180px"
+      width="180px"
     />
     <h2> {props.monster.name} </h2>
     <p>{props.monster.email}</p>
